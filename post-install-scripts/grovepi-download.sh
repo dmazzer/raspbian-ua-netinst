f7596efae3fd05b6ccd3b52ff40ee9df3c14cb74
Daniel Mazzer <dmazzer@gmail.com>
2017-06-15 19:43:44 -0300

    to verify

--- post-install-scripts/grovepi-download.sh
+++ post-install-scripts/grovepi-download.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+#
+# raspbian-ua-netinst post-install-scripts
+# This file is copied to final build and is executed on first root user login.
+#
+# Download and install GrovePi libraries and dependencies.
+#
+# Daniel Mazzer, 2016
+
 
 INSTALL_PATH=/usr/local
 echo "\nGrovePi instalation\n"
@@ -7,6 +15,6 @@ git clone https://github.com/DexterInd/GrovePi $INSTALL_PATH/GrovePi
 cd $INSTALL_PATH/GrovePi/Script
 chmod +x install.sh
 
-echo "\nGrovePi download finished"
-echo "You must run $INSTALL_PATH/GrovePi/Script/install.sh to install required packages\n"
-
+echo "\nGrovePi download finished."
+echo "This script will run $INSTALL_PATH/GrovePi/Script/install.sh to install required packages\n"
+./install.sh
--- post-install-scripts/prepare.sh
+++ post-install-scripts/prepare.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
+#
+# raspbian-ua-netinst post-install-scripts prepare.sh
+# This file is copied to final build and is executed on first root user login.
+#
+# Prepare the system for the first boot.
+#
+# Daniel Mazzer, 2016
+
 echo "prepare has been runned" > /prepare-script-executed
